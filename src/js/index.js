5fc8bfff8efd875e20ea0e01d4687b538f0bbc37
Archangel2903 <archangel2903@gmail.com>
2020-12-17 06:41:26 +0200

    main page

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import 'jquery-ui'
 import 'jquery-ui/ui/effect'
 import 'bootstrap';
+import 'bootstrap-star-rating';
 import 'popper.js';
 import Swiper from 'swiper/dist/js/swiper.min';
 import noUiSlider from 'nouislider';
@@ -13,8 +14,10 @@ $(window).on('load', function () {
     let b = $('body');
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        $('.main-wrapper').slideDown(300);
         b.addClass('ios');
     } else {
+        $('.main-wrapper').slideDown(300);
         b.addClass('web');
     }
 
@@ -87,7 +90,7 @@ $(function () {
                 input.remove();
 
                 noUiSlider.create(elem, {
-                    start: [minValue, maxValue/2],
+                    start: [minValue, maxValue / 2],
                     step: 1,
                     behavior: 'tap-drag',
                     connect: true,
@@ -98,6 +101,57 @@ $(function () {
                 });
             });
         }
+    }
+
+    // Star rating
+    if ($('.rating-input').length) {
+        let rateInput = document.querySelectorAll('.rating-input');
+        rateInput.forEach(function (input) {
+            if (input.classList.contains('rating-read')) {
+                $(input).rating({
+                    showCaption: false,
+                    showClear: false,
+                    displayOnly: true,
+                    max: 5,
+                    size: 'xs',
+                    emptyStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="none" stroke="#FA8D03" stroke-width="21.943" d="M512 47.927l115.19 354.52h372.767L698.382 621.553l2.465 7.581 112.728 346.938L512 756.965 210.427 976.072l115.19-354.519L24.044 402.447h372.765l115.19-354.52z"/></svg>',
+                    filledStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><defs><linearGradient id="logo-gradient" x1="33%" y1="0%" x2="50%" y2="100%" ><stop offset="0%" stop-color="#F1FF0A"></stop><stop offset="100%" stop-color="#FF5C00"></stop></linearGradient></defs><path fill="url(\'#logo-gradient\')" d="M512 25.059l120.868 371.99H1024L707.565 626.952l120.87 371.989L512 769.037 195.566 998.941l120.868-371.989L0 397.049h391.133l120.868-371.99z"/><path fill="url(\'#logo-gradient\')" d="M512 25.059l120.868 371.99H1024L707.565 626.952l120.87 371.989L512 769.037 195.566 998.941l120.868-371.989L0 397.049h391.133l120.868-371.99z"/></svg>',
+                });
+            } else {
+                $(input).rating({
+                    showCaption: false,
+                    showClear: false,
+                    stars: 5,
+                    min: 0,
+                    max: 5,
+                    step: 1,
+                    size: 'xs',
+                    emptyStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="none" stroke="#FA8D03" stroke-width="21.943" d="M512 47.927l115.19 354.52h372.767L698.382 621.553l2.465 7.581 112.728 346.938L512 756.965 210.427 976.072l115.19-354.519L24.044 402.447h372.765l115.19-354.52z"/></svg>',
+                    filledStar: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><defs><linearGradient id="logo-gradient" x1="33%" y1="0%" x2="50%" y2="100%" ><stop offset="0%" stop-color="#F1FF0A"></stop><stop offset="100%" stop-color="#FF5C00"></stop></linearGradient></defs><path fill="url(\'#logo-gradient\')" d="M512 25.059l120.868 371.99H1024L707.565 626.952l120.87 371.989L512 769.037 195.566 998.941l120.868-371.989L0 397.049h391.133l120.868-371.99z"/><path fill="url(\'#logo-gradient\')" d="M512 25.059l120.868 371.99H1024L707.565 626.952l120.87 371.989L512 769.037 195.566 998.941l120.868-371.989L0 397.049h391.133l120.868-371.99z"/></svg>',
+                });
+            }
+        });
+    }
+
+    // Accordion list
+    if ($('.accordion-list').length) {
+        let textBox = $('.accordion-list__box-text'),
+            currentItem = $('.accordion-list__box.active'),
+            currentText = currentItem.find('.accordion-list__box-text');
+
+        textBox.slideUp(0);
+        currentText.slideDown(0);
+
+        $('.accordion-list__box').on('click', function (e) {
+            e.preventDefault();
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active').find('.accordion-list__box-text').slideUp(350);
+            } else {
+                $('.accordion-list__box.active').removeClass('active').find('.accordion-list__box-text').slideUp(350);
+                $(this).addClass('active').find('.accordion-list__box-text').slideDown(350);
+            }
+            e.stopPropagation();
+        });
     }
 
     // Lazy load observer
